e163fdd3efb6febe8d30fbd50e1e239cff257d2e
Hicka <hello@hicka.dev>
2021-12-03 12:43:37 +0100

    Problem 1

---
+++ 1/script.py
@@ -0,0 +1,14 @@
+f = open('input.txt')
+
+count = 0
+line = f.readline()
+last = int(line)
+while line:
+	line = f.readline()
+	if len(line) > 0:
+		depth = int(line)
+		if depth > last:
+			count+=1
+		last = depth
+f.close()
+print(count)
